25e6313ac703e37ba26bdd8e2bb19b55412fabcb
pedrada22 <pmcostaalcantara@gmail.com>
2017-02-14 21:37:31 -0200

    agora uma segunda pagina pag1 com uma notacao que dispensa arquivo web.xml

---
+++ Poo2_14do02de2017/src/java/br/iesb/servlets/pag1.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.iesb.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+        
+/**
+ *
+ * 
+/*
+•	Passo 1: Criar uma classe java sem o método main
+•	Passo 2: Herdar as propriedades da classe HttpServlet
+•	Passo 3: Criar dois métodos com os seguintes nomes 
+            (protected void doPost e protected void doGet)
+o	Passo 3.1: Nos métodos doPost e doGet acrescentar dois parâmetros 
+            (HttpServletRequest request, HttpServletResponse response)
+o	Passo 3.2 Nos métodos doPost e doGet lançar as seguintes exceções: 
+            IOException, ServletException
+•	Passo 4: No corpo do método doGet, chamar o método doPost
+•	Passo 5: Cadastrar a servlet (no arquivo web.xml ou ... ?)
+*/
+
+@WebServlet(name = "ServMath",
+            urlPatterns = "/action/math.do",
+            description = "Servet para executar operações matemáticas")
+public class pag1 extends HttpServlet
+{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html; charset=UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        
+        String varHtml = new String();
+        
+        int num = Integer.parseInt(request.getParameter("fname"));
+        
+        int idx = 0;
+        for (int i=0; i <= num ; i++)
+        {
+            
+            if (i % 2 == 0)
+            {
+                varHtml = varHtml+""+(i)+","+"";
+            }
+            if (i % Math.sqrt(num) == 0 && i != 0)
+            {
+                varHtml = varHtml+"<br>";
+            }
+        }
+        
+        
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title>");
+        out.print("POst Titulo da parada...");
+        out.print("</title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print(varHtml);
+        out.print("</body>");
+        out.print("</html>");
+        
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        
+        PrintWriter out = response.getWriter();
+        
+        String varHtml = new String();
+        
+        varHtml = "<form method=POST>\n" +
+        "  URL: <input type=\"text\" name=\"fname\"><br>\n" +
+        "  <input type=\"submit\" value=\"Submit\">\n" +
+        "</form>";
+        
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title>");
+        out.print("Titulo da parada...");
+        out.print("</title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print(varHtml);
+        out.print("</body>");
+        out.print("</html>");
+        
+                
+                
+    }
+}
